ddd8faea2fafd295afca3cfc74518f23ca1df61a
young <siosio34@nate.com>
2016-12-29 17:04:04 +0900

    init commit

---
+++ app/src/main/java/com/dragon4/owo/ar_trace/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dragon4.owo.ar_trace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
