084fa458e06f5cd602af66886dcba39d7767d57e
young <siosio34@nate.com>
2016-12-31 02:08:59 +0900

    add ar_custom_view layout make and NaverMaplib import

---
+++ app/src/main/java/com/dragon4/owo/ar_trace/NaverMap/FragmentMapActivity.java
@@ -0,0 +1,12 @@
+package com.dragon4.owo.ar_trace.NaverMap;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by joyeongje on 2016. 12. 31..
+ */
+
+public class FragmentMapActivity extends FragmentActivity {
+
+
+}
--- app/src/main/java/com/dragon4/owo/ar_trace/NaverMap/FragmentMapview.java
+++ app/src/main/java/com/dragon4/owo/ar_trace/NaverMap/FragmentMapview.java
@@ -1,8 +1,74 @@
 package com.dragon4.owo.ar_trace.NaverMap;
 
+import android.support.v4.app.Fragment;
+
+import com.dragon4.owo.ar_trace.R;
+import com.nhn.android.maps.NMapContext;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nhn.android.maps.NMapContext;
+import com.nhn.android.maps.NMapView;
+
 /**
  * Created by joyeongje on 2016. 12. 31..
  */
 
-public class FragmentMapview {
+public class FragmentMapview extends Fragment{
+
+    private NMapContext mMapContext;
+    private static final String CLIENT_ID = "FUYe3rcT2vNtJtk4aoK2";// 애플리케이션 클라이언트 아이디 값
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map_view, container, false);
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMapContext =  new NMapContext(super.getActivity());
+        mMapContext.onCreate();
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        NMapView mapView = (NMapView)getView().findViewById(R.id.mapView);
+        mapView.setClientId(CLIENT_ID);// 클라이언트 아이디 설정
+        mMapContext.setupMapView(mapView);
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        mMapContext.onStart();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapContext.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapContext.onPause();
+    }
+    @Override
+    public void onStop() {
+        mMapContext.onStop();
+        super.onStop();
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+    @Override
+    public void onDestroy() {
+        mMapContext.onDestroy();
+        super.onDestroy();
+    }
 }
+
+
+
+
