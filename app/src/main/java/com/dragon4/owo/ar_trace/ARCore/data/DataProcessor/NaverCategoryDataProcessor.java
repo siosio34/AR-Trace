a1c474261087b3bc192da65d98518449ccbdf8fb
young <siosio34@nate.com>
2016-12-31 22:03:01 +0900

    marker load error edit

--- app/src/main/java/com/dragon4/owo/ar_trace/ARCore/data/DataProcessor/NaverCategoryDataProcessor.java
+++ app/src/main/java/com/dragon4/owo/ar_trace/ARCore/data/DataProcessor/NaverCategoryDataProcessor.java
@@ -1,11 +1,16 @@
 package com.dragon4.owo.ar_trace.ARCore.data.DataProcessor;
 
+import android.util.Log;
+
 import com.dragon4.owo.ar_trace.ARCore.ARMarker;
+import com.dragon4.owo.ar_trace.ARCore.SocialARMarker;
 import com.dragon4.owo.ar_trace.ARCore.data.DataSource;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,9 +21,89 @@ public class NaverCategoryDataProcessor implements DataProcessor{
 
 
     @Override
-    public List<ARMarker> load(String rawData, DataSource.DATASOURCE dataformat) throws JSONException {
-        return null;
+    public List<ARMarker> load(String rawData, DataSource.DATASOURCE datasource) throws JSONException {
+
+        List<ARMarker> markers = new ArrayList<ARMarker>();
+        JSONObject root = convertToJSON(rawData);
+        JSONArray dataArray = null;
+
+        if (root.has("result") && root.getJSONObject("result").has("site")) // 연결 가능한 링크를 가졌을시
+            dataArray = root.getJSONObject("result").getJSONArray("site");
+
+
+        else if(root.has("result") && root.getJSONObject("result").has("station") ) { // 버스, 역 마커일경우 따로 처리해줘야됨
+            dataArray = root.getJSONObject("result").getJSONArray("station");
+        }
+
+        if(dataArray == null) {
+            Log.i("Naver Category Error",": data nothing");
+            return null;
+        }
+        JSONObject getObject = null;
+        ARMarker marker = null;
+
+        for(int i = 0 ; i < dataArray.length() ; i++ ) {
+            getObject = dataArray.getJSONObject(i);
+            if (datasource != DataSource.DATASOURCE.BUSSTOP) {
+                marker = processCategoryJsonObject(getObject, datasource);
+            } else {
+                marker = processBusJsonObject(getObject, datasource);
+            }
+            if (marker != null) {
+                markers.add(marker);
+            }
+        }
+
+        return markers;
     }
+
+    public ARMarker processCategoryJsonObject(JSONObject jo, DataSource.DATASOURCE datasource) throws JSONException {
+        ARMarker marker = null;
+
+        String id = jo.getString("id");
+        String naverWebLink = "http://map.naver.com/local/siteview.nhn?code=" + id.substring(1);
+
+        marker = new SocialARMarker(jo.getString("name"),
+                jo.getDouble("y"),
+                jo.getDouble("x"),
+                0,
+                naverWebLink,
+                datasource,
+                datasource.toString());
+
+
+        marker.setID(jo.getString("id"));
+        return marker;
+    }
+
+    public ARMarker processBusJsonObject(JSONObject jo, DataSource.DATASOURCE datasource) throws JSONException {
+        ARMarker marker = null;
+
+        String link = jo.getString("stationDisplayID");
+        String[] sTemp = link.split("-");
+        link = "";
+        String webBusLink;
+
+        for(int i =0 ; i<sTemp.length; i++) {
+            link += sTemp[i];
+        }
+
+        webBusLink = ("http://lab.khlug.org/manapie/bus_arrival.php?station=" + link);
+
+        marker = new SocialARMarker(jo.getString("name"),
+                jo.getDouble("y"),
+                jo.getDouble("x"),
+                0,
+                webBusLink,
+                datasource,
+                datasource.toString());
+
+
+        marker.setID(jo.getString("id"));
+        return marker;
+    }
+
+
 
     private JSONObject convertToJSON(String rawData){
         try {
